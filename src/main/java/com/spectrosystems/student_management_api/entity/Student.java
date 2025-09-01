c74436069ff7da5862b0a49d85e4da645f1a2606
ahmedashraff34 <ahmedalfa254@gmail.com>
2025-09-01 04:43:42 +0300

    Add validation and improve exception handling for Student
    Introduced validation annotations to the Student entity fields and enforced validation in StudentController for add and update operations. Enhanced GlobalExceptionHandler to handle validation errors and database integrity violations, providing clearer error messages for invalid input and duplicate emails.

--- src/main/java/com/spectrosystems/student_management_api/entity/Student.java
+++ src/main/java/com/spectrosystems/student_management_api/entity/Student.java
@@ -1,15 +1,11 @@
 package com.spectrosystems.student_management_api.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
-
 
 @Data
 @Builder
@@ -20,8 +16,21 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
+
+    @NotBlank   (message = "First name is required")
+    @Size       (min = 3, max = 15, message = "First name must be 3-15 characters")
     String firstName;
+
+    @NotBlank   (message = "Last name is required")
+    @Size       (min = 3, max = 15, message = "Last name must be 3-15 characters")
     String lastName;
+
+    @NotBlank   (message = "Email is required")
+    @Email      (message = "Invalid Email address")
+    @Column(unique = true)
     String email;
+
+    @NotNull   (message = "Date of birth is required")
+    @Past       (message = "The date of birth must be in the past")
     LocalDate dateOfBirth;
 }
