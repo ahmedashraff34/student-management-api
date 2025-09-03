81fc8e15a5ee443a9fc1918cf9703cca5f728281
ahmedashraff34 <ahmedalfa254@gmail.com>
2025-09-03 23:21:45 +0300

    Remove unused import in StudentServiceTest
    Cleaned up import statements in StudentServiceTest.java by removing an unnecessary blank line for better code organization.

--- src/test/java/com/spectrosystems/student_management_api/services/StudentServiceTest.java
+++ src/test/java/com/spectrosystems/student_management_api/services/StudentServiceTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
